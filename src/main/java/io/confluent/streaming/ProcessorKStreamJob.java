9d9f0ca84a2c76f911b6e13c0050697cba0ff9a2
Guozhang Wang <wangguoz@gmail.com>
2015-07-27 17:55:45 -0700

    add an example for using local state store

--- src/main/java/io/confluent/streaming/ProcessorKStreamJob.java
+++ src/main/java/io/confluent/streaming/ProcessorKStreamJob.java
@@ -1,16 +1,17 @@
 package io.confluent.streaming;
 
-import org.apache.kafka.common.utils.Utils;
-
 /**
  * Created by guozhang on 7/14/15.
  */
 public abstract class ProcessorKStreamJob<K, V> implements KStreamJob, Processor<K, V> {
 
+  protected KStreamContext streamContext;
+
   @SuppressWarnings("unchecked")
   @Override
   public void init(KStreamContext context) {
-    context.from(null).process((Processor) this);
+    this.streamContext = context;
+    context.from().process((Processor) this);
   }
 
   @Override
